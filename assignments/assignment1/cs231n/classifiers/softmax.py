7a37233a923440c9212a9c522bda00b1cd2c3077
Xueying <xylu@utexas.edu>
2020-11-26 22:05:21 -0600

    softmax

--- assignments/assignment1/cs231n/classifiers/softmax.py
+++ assignments/assignment1/cs231n/classifiers/softmax.py
@@ -33,8 +33,28 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    scores = X.dot(W)
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
 
+    # Softmax loss
+    for i in range(num_train):
+      f = scores[i] - np.max(scores[i])
+      softmax = np.exp(f)/np.sum(np.exp(f))
+      loss += -np.log(softmax[y[i]])
+      
+      # Weight gradient
+      for j in range(num_classes):
+        dW[:,j] += X[i] * softmax[j]
+        dW[:,y[i]] -= X[i]
+
+    # Average
+    loss /= num_train
+    dW /= num_train
+
+    # Regularization
+    loss += reg * np.sum(W*W)
+    dW += 2*reg*W
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
@@ -58,7 +78,13 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    scores = X.dot(W)
+    score -= np.max(scores, axis = 1, keepdims = True)
+    num_train = X.shape[0]
+    
+    sum_exp_scores = np.exp(scores).sum(axis = 1, keepdims = True)
+    softmax_matrix = np.exp(scores)/sum_exp_scores
+    loss = np.sum(- np.log(softmax_matrix[np.arange(num_train), y]))
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
